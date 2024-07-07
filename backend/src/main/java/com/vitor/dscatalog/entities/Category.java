6d72a1cf4a3e7d65ea2809da88a38d4c15224f72
vitorhugosdc <vitorvitinho9900@hotmail.com>
2024-07-07 14:20:26 -0300

    Adicionando dados de auditoria

--- backend/src/main/java/com/vitor/dscatalog/entities/Category.java
+++ backend/src/main/java/com/vitor/dscatalog/entities/Category.java
@@ -1,11 +1,15 @@
 package com.vitor.dscatalog.entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
 
+import java.time.Instant;
 import java.util.Objects;
 
 @Entity
@@ -16,6 +20,12 @@ public class Category {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    /*Dado de auditoria para saber o instante em que esse registro foi criado*/
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant createdAt;
+    /*Dado de auditoria para saber o instante em que esse registro foi alterado*/
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant updatedAt;
 
     public Category() {
     }
@@ -39,6 +49,28 @@ public class Category {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    /*NÃO criados os Sets desses dados de auditoria, pois serão gerados AUTOMATICAMENTE pelos métodos abaixo ao criar e alterar os dados respectivamente*/
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+
+    /*@PrePersist é uma anotação padrão que será executada AUTOMATICAMENTE ao executar um SAVE do JPA pela PRIMEIRA VEZ
+     * ex: category.save()*/
+    @PrePersist
+    public void prePersist() {
+        createdAt = Instant.now();
+    }
+
+    /*O mesmo que o acima, porém para um SAVE que NÃO é a PRIMEIRA VEZ*/
+    @PreUpdate
+    public void preUpdate() {
+        updatedAt = Instant.now();
     }
 
     @Override
