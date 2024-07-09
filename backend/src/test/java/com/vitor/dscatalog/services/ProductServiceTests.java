e0da971aa0d5fd737e2b0dcb6e351cec22c344d3
vitorhugosdc <vitorvitinho9900@hotmail.com>
2024-07-09 11:32:10 -0300

    ProductServiceTests findAll

--- backend/src/test/java/com/vitor/dscatalog/services/ProductServiceTests.java
+++ backend/src/test/java/com/vitor/dscatalog/services/ProductServiceTests.java
@@ -1,17 +1,28 @@
 package com.vitor.dscatalog.services;
 
+import com.vitor.dscatalog.dto.ProductDTO;
+import com.vitor.dscatalog.entities.Product;
 import com.vitor.dscatalog.repositories.ProductRepository;
 import com.vitor.dscatalog.services.exceptions.DatabaseException;
 import com.vitor.dscatalog.services.exceptions.ResourceNotFoundException;
+import com.vitor.dscatalog.tests.Factory;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+import java.util.Optional;
 
 /*Não carrega o contexto, mas permite usar os recursos do Spring com JUnit (teste de unidade: service/component)*/
 @ExtendWith(SpringExtension.class)
@@ -41,6 +52,8 @@ public class ProductServiceTests {
     private long existingId;
     private long nonExistingId;
     private long dependentId;
+    private PageImpl<Product> page;
+    private Product product;
 
     @BeforeEach
     void setUp() throws Exception {
@@ -49,6 +62,8 @@ public class ProductServiceTests {
         existingId = 1L;
         nonExistingId = 1000L;
         dependentId = 3L;
+        product = Factory.createProduct();
+        page = new PageImpl<>(List.of(product));
 
         /*CONFIGURAÇÃO DO NOSSO MOCK DO REPOSITORY*/
         /*Não é necessário colocar Mockito.algumMetodo()
@@ -60,15 +75,31 @@ public class ProductServiceTests {
         Mockito.when(repository.existsById(nonExistingId)).thenReturn(false);
         Mockito.when(repository.existsById(dependentId)).thenReturn(true);
 
-        /*Basicamente: quando eu chamar o deleteById com um id existente, esse método não vai fazer nada, oq é o esperado*/
+        /*Basicamente: quando eu chamar o deleteById com um id existente, esse método não vai fazer nada, oq é o esperado
+         * QUANDO O MÉTODO RETORNA VOID, a gente coloca PRIMEIRO a ação e DEPOIS o WHEN, igual abaixo, donothing vem antes de when
+         * AGORA, quando o método RETORNA alguma coisa, ai inverte, primeiro vem o WHEN, depois a ação*/
+
         Mockito.doNothing().when(repository).deleteById(existingId);
         /*Por que DataIntegrityViolationException ao invés de DatabaseException? pois no meu service, o REPOSITORY (dependencia) lança DataIntegrityViolationException
            Então, temos que simular o comportamento desse repository lançando DataIntegrityViolationException
            pois lançar DatabaseException é do SERVICE, que é oq estamos testando*/
         Mockito.doThrow(DataIntegrityViolationException.class).when(repository).deleteById(dependentId);
 
-        /*Perceba que para o método delete, ele tem 2 métodos da dependência, o existsById e o deleteBYid do ProductRepository.
-         * Então tivemos que configurar o comportamento simulado de cada um deles*/
+        /*Perceba que para o método delete, ele tem 2 métodos da dependência, o existsById e o deleteById do ProductRepository.
+         * Então tivemos que configurar o comportamento simulado de cada um deles em cada uma das situações
+         *
+         * O findById também, TODOs os método que acessa o REPOSITORY temos que simular qual seria o comportamento deles*/
+
+        /*Quando o método possui uma SOBRECARGA, como o findAll do JPA, temos que especificar qual o tipo, no caos, Pageable com um cast*/
+        Mockito.when(repository.findAll((Pageable) ArgumentMatchers.any())).thenReturn(page);
+
+        Mockito.when(repository.save(ArgumentMatchers.any())).thenReturn(product);
+
+        /*Optional de produto
+         * Estamos testando os 2 casos do findById, id existente e não existente*/
+        Mockito.when(repository.findById(existingId)).thenReturn(Optional.of(product));
+
+        Mockito.when((repository.findById(nonExistingId))).thenReturn(Optional.empty());
     }
 
     @Test
@@ -89,6 +120,8 @@ public class ProductServiceTests {
     public void deleteShouldThrowResourceNotFoundExceptionWhenIdDoesNotExists() {
 
         Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+            /*Lá na configuração de simulação do mock, definimos que é pro existsById retornar false quando for nonExistingId
+             * Por isso é de se esperar que seja lançada uma exceção*/
             service.delete(nonExistingId);
         });
     }
@@ -99,5 +132,15 @@ public class ProductServiceTests {
         Assertions.assertThrows(DatabaseException.class, () -> {
             service.delete(dependentId);
         });
+    }
+
+    @Test
+    public void findAllShoudReturnPageOfProducts() {
+
+        Pageable pageable = PageRequest.of(0, 10);
+        Page<ProductDTO> result = service.findAll(pageable);
+        Assertions.assertNotNull(result);
+        /*Verificando se o repositório foi chamado corretamente, 1x no caso*/
+        Mockito.verify(repository, Mockito.times(1)).findAll(pageable);
     }
 }
